34237aeac9975716afad82a25c64767d52a994a3
Paul Grosen <Paul Grosen>
2016-02-04 12:19:41 -0800

    Improved web interface, added jscpp integration

--- js/main.js
+++ js/main.js
@@ -1,3 +1,34 @@
 var editor = ace.edit("editor");
-editor.setTheme("ace/theme/chromevox");
 editor.getSession().setMode("ace/mode/c_cpp");
+
+var code = `#include <iostream>
+using namespace std;
+int main() {
+    int x;
+    cin >> x;
+    cout << x << endl;
+    return 0;
+}`;
+
+editor.setValue(code);
+
+editor.commands.addCommand({
+   name: "run",
+   bindKey: {win: "Ctrl-Enter", mac: "Ctrl-Enter"},
+   exec: runCode
+});
+
+function runCode() {
+	var code = 	editor.getValue();
+	var input = "4321";
+	var output = "";
+	var config = {
+		stdio: {
+			write: function(s) {
+				output += s;
+			}
+		}
+	};
+	var exitCode = JSCPP.run(code, input, config);
+	document.getElementById("console-output").innerHTML = output;
+}
