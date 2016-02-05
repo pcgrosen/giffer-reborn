c8f8625fb8916606dc210d2418e1f0347714d2b6
Paul Grosen <pcgrosen@gmail.com>
2016-02-04 21:54:28 -0800

    Began implementing String, HardwareSerial, Stream, and Print

--- js/Arduino.js
+++ js/Arduino.js
@@ -1,5 +1,50 @@
 var load = function(rt) {
   console.log("hello, world!");
+  
+  // STRING ///////////////////////////////////////////////////////
+  //Define type
+  var string_t = rt.newClass("String", [
+    {
+      type: rt.normalPointerType(rt.charTypeLiteral),
+      name: "buffer"
+    }, {
+      type: rt.unsignedintTypeLiteral,
+      name: "capacity"
+    }, {
+      type: rt.unsignedintTypeLiteral,
+      name: "len"
+    }
+  ]);
+  rt.types[rt.getTypeSignature(string_t)] = {
+    "#father": "object"
+  };
+  
+  
+  // SERIAL ///////////////////////////////////////////////////////
+  //Define types
+  var print_t = rt.newClass("Print", []);
+  rt.types[rt.getTypeSignature(print_t)] = {
+    "#father": "object"
+  };
+  var stream_t = rt.newClass("Stream", []);
+  rt.types[rt.getTypeSignature(stream_t)] = {
+    "#father": "Print"
+  };
+  var hs_t = rt.newClass("HardwareSerial", []);
+  rt.types[rt.getTypeSignature(hs_t)] = {
+    "#father": "Stream"
+  };
+
+  //Create serial object
+  var serial_obj = {
+    t: hs_t,
+    v: {
+      members: {}
+    },
+    left: false
+  };
+  rt.scope[0]["Serial"] = serial_obj;
+  
 }
 
 arduino_h = {
