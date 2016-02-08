0ac8ccd8a2562e931330063980c83c7ee38fc963
Paul Grosen <pcgrosen@gmail.com>
2016-02-07 18:56:06 -0800

    Added LED functions and basic frame functionality

--- js/Arduino.js
+++ js/Arduino.js
@@ -1,5 +1,126 @@
+LOW = 0x0
+HIGH = 0x1
+
+INPUT = 0x0
+OUTPUT = 0x1
+INPUT_PULLUP = 0x2
+
+function Frame(previousFrame) {
+  if (typeof(previousFrame) === "undefined") {
+    this.ledModes = {}
+    this.ledStates = {}
+    this.postDelay = 0
+  } else {
+    this.ledModes = {}
+    this.ledStates = {}
+    for (var prop in previousFrame.ledModes) {
+      this.ledModes[prop] = previousFrame.ledModes[prop]
+    }
+    for (var prop in previousFrame.ledStates) {
+      this.ledStates[prop] = previousFrame.ledStates[prop]
+    }
+    this.postDelay = 0
+  }
+}
+
+Frame.prototype.getPinMode = function (pinNumber) {
+  if (typeof(this.pinModes[pinNumber]) === "undefined") {
+    return INPUT
+  } else {
+    return this.pinModes[pinNumber]
+  }
+}
+
+Frame.prototype.setPinMode = function (pinNumber, mode) {
+  this.ledModes[pinNumber] = mode
+}
+
+Frame.prototype.getPinState = function (pinNumber) {
+  if (typeof(this.pinStates[pinNumber]) === "undefined") {
+    return LOW
+  } else {
+    return this.pinStates[pinNumber]
+  }
+}
+
+Frame.prototype.setPinState = function (pinNumber, state) {
+  this.ledStates[pinNumber] = state
+}
+
+function FrameManager() {
+  this.frames = []
+  this.frames[0] = new Frame()
+  this.currentFrame = 0
+}
+
+FrameManager.prototype.getPinMode = function (pinNumber, frame) {
+  if (typeof(frame) === "undefined") {
+    this.frames[this.currentFrame].getPinMode(pinNumber)
+  } else {
+    this.frames[frame].getPinMode(pinNumber)
+  }
+}
+
+FrameManager.prototype.setPinMode = function (pinNumber, mode) {
+  this.frames[this.currentFrame].setPinMode(pinNumber, mode)
+}
+
+FrameManager.prototype.getPinState = function (pinNumber, frame) {
+  if (typeof(frame) === "undefined") {
+    this.frames[this.currentFrame].getPinState(pinNumber)
+  } else {
+    this.frames[frame].getPinState(pinNumber)
+  }
+}
+
+FrameManager.prototype.setPinState = function (pinNumber, state) {
+  this.frames[this.currentFrame].setPinState(pinNumber, state)
+}
+
+FrameManager.prototype.nextFrame = function (delay) {
+  this.frames[this.currentFrame].postDelay = delay
+  this.frames.push(new Frame(this.frames[this.currentFrame]))
+  this.currentFrame++
+}
+
 var load = function(rt) {
-  console.log("hello, world!");
+  console.log("hello, world!")
+  
+  // LED FUNCTIONS ////////////////////////////////////////////////
+  
+  rt.scope[0]["LOW"] = LOW
+  rt.scope[0]["HIGH"] = HIGH
+  
+  rt.scope[0]["INPUT"] = INPUT
+  rt.scope[0]["OUTPUT"] = OUTPUT
+  rt.scope[0]["INPUT_PULLUP"] = INPUT_PULLUP
+  
+  frameManager = new FrameManager()
+  
+  var pinMode = function (rt, _this, pinNumber, mode) {
+    if (mode > 2) {
+      rt.raiseException("Unknown mode " + mode.toString())
+      return
+    }
+    frameManager.setPinMode(pinNumber.v, mode.v)
+  }
+  rt.regFunc(pinMode, "global", "pinMode", [rt.unsignedintTypeLiteral, rt.unsignedintTypeLiteral], rt.voidTypeLiteral)
+  
+  var digitalWrite = function (rt, _this, pinNumber, state) {
+    if (state > 1) {
+      rt.raiseException("Unknown state " + state.toString())
+      return
+    }
+    frameManager.setPinState(pinNumber.v, state.v)
+  }
+  rt.regFunc(digitalWrite, "global", "digitalWrite", [rt.unsignedintTypeLiteral, rt.unsignedintTypeLiteral], rt.voidTypeLiteral)
+  
+  // DELAY ////////////////////////////////////////////////////////
+  
+  var delay = function (rt, _this, ms) {
+    frameManager.nextFrame(ms.v)
+  }
+  rt.regFunc(delay, "global", "delay", [rt.primitiveType("unsigned long")], rt.voidTypeLiteral)
   
   // STRING ///////////////////////////////////////////////////////
   //Define type
@@ -14,30 +135,30 @@ var load = function(rt) {
       type: rt.unsignedintTypeLiteral,
       name: "len"
     }
-  ]);
+  ])
   rt.types[rt.getTypeSignature(string_t)] = {
     "#father": "object"
-  };
-  var to_char_star = function(rt, _this) {
-	  return _this.v.members.buffer;
   }
-  rt.regFunc(to_char_star, string_t, "toCharStar", [], rt.normalPointerType(rt.charTypeLiteral));
+  var to_char_star = function(rt, _this) {
+    return _this.v.members.buffer
+  }
+  rt.regFunc(to_char_star, string_t, "toCharStar", [], rt.normalPointerType(rt.charTypeLiteral))
   
   
   // SERIAL ///////////////////////////////////////////////////////
   //Define types
-  var print_t = rt.newClass("Print", []);
+  var print_t = rt.newClass("Print", [])
   rt.types[rt.getTypeSignature(print_t)] = {
     "#father": "object"
-  };
-  var stream_t = rt.newClass("Stream", []);
+  }
+  var stream_t = rt.newClass("Stream", [])
   rt.types[rt.getTypeSignature(stream_t)] = {
     "#father": "Print"
-  };
-  var hs_t = rt.newClass("HardwareSerial", []);
+  }
+  var hs_t = rt.newClass("HardwareSerial", [])
   rt.types[rt.getTypeSignature(hs_t)] = {
     "#father": "Stream"
-  };
+  }
 
   //Create serial object
   var serial_obj = {
@@ -46,8 +167,8 @@ var load = function(rt) {
       members: {}
     },
     left: false
-  };
-  rt.scope[0]["Serial"] = serial_obj;
+  }
+  rt.scope[0]["Serial"] = serial_obj
   
 }
 
--- js/main.js
+++ js/main.js
@@ -1,40 +1,49 @@
-var editor = ace.edit("editor");
-editor.getSession().setMode("ace/mode/c_cpp");
+var editor = ace.edit("editor")  
+editor.getSession().setMode("ace/mode/c_cpp")  
 
-var code = `#include <iostream>
-#include "Arduino.h"
-using namespace std;
-int main() {
-    int x;
-    cin >> x;
-    cout << x << endl;
-    return 0;
-}`;
+var code = `
+void setup() {
+    
+}
 
-editor.setValue(code);
+void loop() {
+    
+}`
+
+editor.setValue(code)  
+editor.focus()
 
 editor.commands.addCommand({
    name: "run",
    bindKey: {win: "Ctrl-Enter", mac: "Ctrl-Enter"},
    exec: runCode
-});
+})  
 
-console.log(arduino_h);
+console.log(arduino_h)  
+
+running = false
 
 function runCode() {
-  var code = editor.getValue();
-  var input = "4321";
-  var output = "";
+  if (running) {
+    return
+  }
+  running = true
+  var code = "#include \"Arduino.h\"\n" + editor.getValue() + "\n\nint main() { setup(); loop(); return 0;}\n"
+  //var code = editor.getValue()
+  var input = "4321"  
+  var output = ""  
   var config = {
     stdio: {
       write: function(s) {
-        output += s;
+        output += s  
       }
     },
     includes: {
       "Arduino.h": arduino_h //defined in Arduino.js
     }
-  };
-  var exitCode = JSCPP.run(code, input, config);
-  document.getElementById("console-output").innerHTML = output;
+  }  
+  var exitCode = JSCPP.run(code, input, config)  
+  document.getElementById("console-output").innerHTML = JSON.stringify(frameManager)
+  console.log(frameManager)
+  running = false
 }
