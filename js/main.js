ec08dfa6b83a3c3fb47f6588f6beba8334f0ce7b
Paul Grosen <pcgrosen@gmail.com>
2016-02-07 19:28:33 -0800

    Began adding support for generating gif

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 var editor = ace.edit("editor")  
 editor.getSession().setMode("ace/mode/c_cpp")  
 
-var code = `
+var code
+if (typeof(localStorage.code) === "undefined") {
+  code = `
 void setup() {
     
 }
@@ -9,8 +11,11 @@ void setup() {
 void loop() {
     
 }`
+} else {
+  code = localStorage.code
+}
 
-editor.setValue(code)  
+editor.setValue(code)
 editor.focus()
 
 editor.commands.addCommand({
@@ -46,4 +51,8 @@ function runCode() {
   document.getElementById("console-output").innerHTML = JSON.stringify(frameManager)
   console.log(frameManager)
   running = false
+}
+
+function saveCode() {
+  localStorage.code = editor.getValue()
 }
