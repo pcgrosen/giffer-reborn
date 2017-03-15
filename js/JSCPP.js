452d3c673c4fd323776f933eb8aad10b5dab7308
pcgrosen <pcgrosen@gmail.com>
2017-03-15 12:33:01 -0700

    Fix "const" with typedef'ed types

--- js/JSCPP.js
+++ js/JSCPP.js
@@ -19980,6 +19980,8 @@ CRuntime.prototype.simpleType = function(type) {
   } else {
     if (this.isPrimitiveType(type)) {
       return this.primitiveType(type);
+    } else if (type in this.typdefs) {
+      return this.simpleType(this.typdefs[type]);
     } else {
       clsType = {
         type: "class",
